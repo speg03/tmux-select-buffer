af615d54f23394bc12829cb8412110c64423548b
Takahiro Yano <speg03@gmail.com>
2018-04-15 02:28:06 +0900

    :hammer: Refactor

--- scripts/select_buffer.sh
+++ scripts/select_buffer.sh
@@ -4,23 +4,25 @@
 
 select_buffer() {
     local select_command="$1"
-    local select_command_base
     local buffer
 
-    select_command_base=$(echo "$select_command" | cut -d' ' -f1)
-    if type "$select_command_base" &>/dev/null; then
-        buffer=$(tmux list-buffers | $select_command | cut -d: -f1)
-        if [[ $buffer ]]; then
-            tmux paste-buffer -b "$buffer"
-        fi
-    else
-        tmux choose-buffer
+    buffer=$(tmux list-buffers | $select_command | cut -d: -f1)
+    if [[ $buffer ]]; then
+        tmux paste-buffer -b "$buffer"
     fi
 }
 
 
 main() {
-    select_buffer "$@"
+    local select_command="$1"
+    local select_command_base
+
+    select_command_base=$(echo "$select_command" | cut -d' ' -f1)
+    if type "$select_command_base" &>/dev/null; then
+        select_buffer "$select_command"
+    else
+        tmux choose-buffer
+    fi
 }
 
 main "$@"
