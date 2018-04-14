4150974e335cec1c9b60420260f1d638fc48c357
Takahiro Yano <speg03@gmail.com>
2018-04-15 01:59:04 +0900

    :children_crossing: Fall through select command
    select commandが存在しなければデフォルトのtmux choose-bufferを使用する。

--- scripts/select_buffer.sh
+++ scripts/select_buffer.sh
@@ -4,11 +4,17 @@
 
 select_buffer() {
     local select_command="$1"
+    local select_command_base
     local buffer
 
-    buffer=$(tmux list-buffers | $select_command | cut -d: -f1)
-    if [[ $buffer ]]; then
-        tmux paste-buffer -b "$buffer"
+    select_command_base=$(echo "$select_command" | cut -d' ' -f1)
+    if type "$select_command_base" &>/dev/null; then
+        buffer=$(tmux list-buffers | $select_command | cut -d: -f1)
+        if [[ $buffer ]]; then
+            tmux paste-buffer -b "$buffer"
+        fi
+    else
+        tmux choose-buffer
     fi
 }
 
